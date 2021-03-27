edcac3302b52f45dcf7c7656505cc14af519b918
Matt <matiasm@avalith.net>
2021-03-27 14:08:15 -0300

    pom

--- src/main/java/com/example/demo/Controllers/UserController.java
+++ src/main/java/com/example/demo/Controllers/UserController.java
@@ -1,27 +1,36 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.dto.CountriesName;
 import com.example.demo.dto.UserWithEmails;
 import com.example.demo.entitys.User;
 import com.example.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(path = "/user")
 public class UserController {
 
+    @Autowired
     UserService firstService;
 
-    @GetMapping
-    public UserWithEmails findNumber(){
-        return firstService.algo(1);
+    @GetMapping(path = "/algo")
+    public ResponseEntity<User> findString(){
+
+        return ResponseEntity.ok(User.builder()
+                .nombre("Pimpon")
+                .build());
     }
 
-    @PostMapping
-    public String findString(){
-        return "hbfuidsguf";
+    @GetMapping(path = "/paises")
+    public ResponseEntity<List<String>> findCountrys(){
+
+        return ResponseEntity.ok(firstService.findCountrys());
     }
 
 }
--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,10 +1,19 @@
 package com.example.demo.services;
 
+import com.example.demo.dto.Countries;
+import com.example.demo.dto.CountriesName;
 import com.example.demo.dto.UserWithEmails;
 import com.example.demo.entitys.User;
-import com.example.demo.repositorys.EmailRepository;
 import com.example.demo.repositorys.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -12,6 +21,25 @@ public class UserService {
     UserRepository userRepository;
 
     EmailService emailService;
+
+
+    private RestTemplate restTemplate;
+
+
+    public List<String> findCountrys(){
+
+        ResponseEntity<Object[]> responseEntity = restTemplate.getForEntity("https://restcountries.eu/rest/v2/all", Object[].class);
+
+        Object[] objects = responseEntity.getBody();
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        return Arrays.stream(objects)
+                .map(object -> mapper.convertValue(object, CountriesName.class))
+                .map(CountriesName::getName)
+                .collect(Collectors.toList());
+
+    }
 
     public UserWithEmails algo(Integer id){
 
